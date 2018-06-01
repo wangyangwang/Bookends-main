6b67aacbc42b7963181cb97ebb262fa6873eb13c
yang wang <yang.wang.dec@gmail.com>
2018-05-31 22:42:09 -0400

    added red panda testing scene

---
+++ Assets/Scripts/JointParticle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointParticle : MonoBehaviour {
+
+    ParticleSystem ps;
+
+    public static JointParticle instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        ps = GetComponent<ParticleSystem>();
+        
+	}
+
+    public void MakeParicle(Vector3 position)
+    {
+        ps.startColor = Color.HSVToRGB(Random.Range(0f, 1f), 1, 1);
+        gameObject.transform.position = position;
+        ps.Emit(1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
