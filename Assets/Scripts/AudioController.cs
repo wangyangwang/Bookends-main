f36bb500156e383ff5b68b7bd88631c5f0f623ae
Wāng Yáng <yang.wang.dec@gmail.com>
2018-06-09 15:48:43 -0400

    audio controller

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioController : MonoBehaviour {
+
+
+    public static AudioController instance;
+
+    AudioSource audioSource;
+
+    
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Play()
+    {
+        audioSource.Play();
+    }
+
+    public void Pause()
+    {
+        audioSource.Pause();
+    }
+
+    public void Stop()
+    {
+        audioSource.Stop();
+    }
+}
