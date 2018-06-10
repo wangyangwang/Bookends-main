9b3c7683927ba852a9291f742ba400cb6f11cbfc
anyemelody <anyemelody@gmail.com>
2018-06-10 18:29:03 -0400

    Add particleController
    1. Add particle effect package "Kvant";
    2. Add particleController script;
    3. Add particleScene as example;
    4. Add ParticleController prefab.

---
+++ Assets/Scripts/PScontroller.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PScontroller : MonoBehaviour {
+    GameObject[] psChildren;
+    Kvant.Spray[] sprays;
+    int childrenNum;
+    public Slider amountSlider;
+    public Toggle[] enableToggles;
+
+
+    // Use this for initialization
+    void Awake () {
+        childrenNum = transform.childCount;
+        if (childrenNum > 0)
+        {
+            psChildren = new GameObject [childrenNum];
+            sprays = new Kvant.Spray[childrenNum];
+        }
+        
+    }
+
+
+    private void Start()
+    {
+        for (int i = 0; i < childrenNum; i++)
+        {
+            psChildren[i] = gameObject.transform.GetChild(i).gameObject;
+            sprays[i] = psChildren[i].GetComponent<Kvant.Spray>();
+            
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+        ChangeParicleAmount();
+        EnableParticle();
+	}
+
+
+    public void ChangeParicleAmount()
+    {
+        for (int i = 0; i < childrenNum; i++)
+        {
+            sprays[i].throttle = amountSlider.value;
+        }
+
+    }
+
+    public void EnableParticle()
+    {
+        for (int i = 0; i < childrenNum; i++)
+        {
+            sprays[i].enabled = enableToggles[i].isOn;
+        } 
+    }
+}
